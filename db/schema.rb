42c1ba81b0aaa286d2e74fa6e972f542450c340a
YuTingLan <fortitude0623@gmail.com>
2023-06-15 21:14:26 +0800

    change database to postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2023_06_15_085726) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "users", force: :cascade do |t|
     t.string "uid"
     t.string "name"
